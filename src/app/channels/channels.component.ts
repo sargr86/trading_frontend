c54dfc094d6948528f5de370c4da58e4e3d7fc6f
Davit Burton <davitburton280@gmail.com>
2020-09-07 16:57:45 +0400

    Added channels page, chart.js

--- src/app/channels/channels.component.ts
+++ src/app/channels/channels.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CHART_1, CHART_2, CHART_3, CHART_4} from '../core/constants/charts';
+import {Chart} from 'chart.js';
 
 @Component({
   selector: 'app-channels',
@@ -6,10 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./channels.component.scss']
 })
 export class ChannelsComponent implements OnInit {
+  canvas: any;
+  ctx: any;
+  lineChart;
 
-  constructor() { }
+  constructor() {
+
+
+
+  }
 
   ngOnInit(): void {
+    this.canvas = document.getElementById('myChart');
+    this.ctx = this.canvas.getContext('2d');
+    this.lineChart = new Chart(this.ctx, CHART_1);
+
+    this.canvas = document.getElementById('myChart2');
+    this.ctx = this.canvas.getContext('2d');
+    this.lineChart = new Chart(this.ctx, CHART_2);
+
+    this.canvas = document.getElementById('myChart3');
+    this.ctx = this.canvas.getContext('2d');
+    this.lineChart = new Chart(this.ctx, CHART_3);
+
+    this.canvas = document.getElementById('myChart4');
+    this.ctx = this.canvas.getContext('2d');
+    this.lineChart = new Chart(this.ctx, CHART_4);
   }
 
 }

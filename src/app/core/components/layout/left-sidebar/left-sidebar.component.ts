c28a0e6e00f9cb50d9cab7b7d385edbe6962bba4
Davit Burton <davitburton280@gmail.com>
2020-09-08 16:05:46 +0400

    Showing nav bar and sidebar if not login page

--- src/app/core/components/layout/left-sidebar/left-sidebar.component.ts
+++ src/app/core/components/layout/left-sidebar/left-sidebar.component.ts
@@ -26,14 +26,14 @@ export class LeftSidebarComponent implements OnInit {
 
   }
 
-  moveItemInArray(arr, new_index, old_index) {
-    if (new_index >= arr.length) {
-      let k = new_index - arr.length + 1;
+  moveItemInArray(arr, newIndex, oldIndex) {
+    if (newIndex >= arr.length) {
+      let k = newIndex - arr.length + 1;
       while (k--) {
         arr.push(undefined);
       }
     }
-    arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+    arr.splice(newIndex, 0, arr.splice(oldIndex, 1)[0]);
     return arr; // for testing
   }
 

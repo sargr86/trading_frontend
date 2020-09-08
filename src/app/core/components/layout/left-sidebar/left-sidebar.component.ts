ca399313bc65f704a19ab27154fbe3500d8e944f
Daniel <danieldukas11@gmail.com>
2020-09-08 15:45:34 +0400

    Added cdk-drag-and-drop without material, managed to have drag & drop for test list in left-sidebar

--- src/app/core/components/layout/left-sidebar/left-sidebar.component.ts
+++ src/app/core/components/layout/left-sidebar/left-sidebar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CdkDragDrop} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-left-sidebar',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./left-sidebar.component.scss']
 })
 export class LeftSidebarComponent implements OnInit {
+  sampleList = [
+    {name: 'Chillhop Music 1 ', description: 'Lorem ipsum dolor sit amet, consetetur'},
+    {name: 'Chillhop Music 2 ', description: 'Lorem ipsum dolor sit amet, consetetur'},
+    {name: 'Chillhop Music 3', description: 'Lorem ipsum dolor sit amet, consetetur'},
+    {name: 'Chillhop Music 4 ', description: 'Lorem ipsum dolor sit amet, consetetur'},
+    {name: 'Chillhop Music 5', description: 'Lorem ipsum dolor sit amet, consetetur'},
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    this.sampleList = this.moveItemInArray(this.sampleList, event.previousIndex, event.currentIndex);
+
+  }
+
+  moveItemInArray(arr, new_index, old_index) {
+    if (new_index >= arr.length) {
+      let k = new_index - arr.length + 1;
+      while (k--) {
+        arr.push(undefined);
+      }
+    }
+    arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+    return arr; // for testing
   }
 
 }

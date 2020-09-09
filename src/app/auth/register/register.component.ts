3489a22fc5e39179a9392fd6bb85be12874b012e
Davit Burton <davitburton280@gmail.com>
2020-09-09 17:26:28 +0400

    Added login/register functionality

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,18 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Subscription} from 'rxjs';
+import {AuthService} from '@core/services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
+  registerForm: FormGroup;
+  subscriptions: Subscription[] = [];
 
   constructor(
-    public router: Router
-  ) { }
+    public router: Router,
+    private fb: FormBuilder,
+    private auth: AuthService
+  ) {
+  }
 
   ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      full_name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  register() {
+    this.auth.register(this.registerForm.value).subscribe((dt: any) => {
+      localStorage.setItem('token', (dt.hasOwnProperty('token') ? dt.token : ''));
+      this.router.navigate(['/']);
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(s => s.unsubscribe());
   }
 
 }
---
+++ src/app/shared/models/user.ts
@@ -0,0 +1,12 @@
+export class User {
+  id?: number;
+  first_name?: string;
+  last_name?: string;
+  birthday?: string;
+  gender?: string;
+  email?: string;
+  password?: string;
+  profile_img?: string;
+  token?: string;
+  roles?: string[];
+}

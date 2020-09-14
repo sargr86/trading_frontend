fd4868e94f2fa7c0482fadee55a3c67bacf535e7
Daniel <danieldukas11@gmail.com>
2020-09-14 16:24:22 +0400

    Installed videojs with recording info

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CarouselModule} from 'ngx-owl-carousel-o';
 import { VideoJsComponent } from './components/video-js/video-js.component';
+import { VideoJsRecordComponent } from './components/video-js-record/video-js-record.component';
 
 
 @NgModule({
   declarations: [
-    VideoJsComponent
+    VideoJsComponent,
+    VideoJsRecordComponent
   ],
   imports: [
     CommonModule,
@@ -14,7 +16,8 @@ import { VideoJsComponent } from './components/video-js/video-js.component';
   ],
   exports: [
     CarouselModule,
-    VideoJsComponent
+    VideoJsComponent,
+    VideoJsRecordComponent
   ]
 })
 export class SharedModule {

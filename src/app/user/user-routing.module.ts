0bf148890578996440b9331df355c5ac0325f827
Davit Burton <davitburton280@gmail.com>
2020-09-14 11:58:51 +0400

    Installed videojs without recording info, but with working example

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,12 +1,17 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {ProfileComponent} from './profile/profile.component';
+import {VideoComponent} from '@app/user/video/video.component';
 
 
 const routes: Routes = [
   {
     path: 'profile',
     component: ProfileComponent
+  },
+  {
+    path: 'video',
+    component: VideoComponent
   }
 ];
 

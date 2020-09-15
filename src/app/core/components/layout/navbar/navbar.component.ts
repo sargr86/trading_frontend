47f8d5802381f489b838d2deb97098f20be8504c
Daniel <danieldukas11@gmail.com>
2020-09-15 11:08:57 +0400

    Moved crypto currency component in core module

--- src/app/core/components/layout/navbar/navbar.component.ts
+++ src/app/core/components/layout/navbar/navbar.component.ts
@@ -23,7 +23,7 @@ export class NavbarComponent implements OnInit {
   }
 
   openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template);
+    this.modalRef = this.modalService.show(template, { class: 'modal-lg' });
   }
 
 }

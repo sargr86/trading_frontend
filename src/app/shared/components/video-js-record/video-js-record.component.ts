fcb0836ffa929b81b900d0d08f563cf6f02fe76a
Davit Burton <davitburton280@gmail.com>
2020-11-11 14:31:26 +0400

    Video recording fix

--- src/app/shared/components/video-js-record/video-js-record.component.ts
+++ src/app/shared/components/video-js-record/video-js-record.component.ts
@@ -145,6 +145,9 @@ export class VideoJsRecordComponent implements OnInit, OnDestroy, AfterViewInit 
             console.log(this.openViduToken)
             this.recordingState = 'active';
             this.subject.setVideoRecordingState({recording: true, viaSocket: false});
+            console.log('RECORDING!!!!')
+            console.log(this.authUser)
+            console.log('RECORDING!!!!')
             this.videoService.saveVideoToken({
                 token: this.openViduToken,
                 author_id: this.authUser.id,

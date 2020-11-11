401401e99f000faa82808d75f1417a0b0009d934
David Burton <davitburton280@gmail.com>
2020-11-11 14:09:08 +0400

    Open video player from home page

--- src/app/user/home/home.component.ts
+++ src/app/user/home/home.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {OwlOptions} from 'ngx-owl-carousel-o';
 import {API_URL, OWL_OPTIONS} from '@core/constants/global';
 import {VideoService} from '@core/services/video.service';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-home',
@@ -14,7 +15,8 @@ export class HomeComponent implements OnInit {
     apiUrl = API_URL;
 
     constructor(
-        private videoService: VideoService
+        private videoService: VideoService,
+        public router: Router
     ) {
     }
 
@@ -25,7 +27,19 @@ export class HomeComponent implements OnInit {
     }
 
     openVideoPage(video, username) {
+        console.log(username);
+        let route;
+        let params;
+        if (video.status === 'live') {
+            route = 'user/video/watch';
+            params = {session: video.session_name, publisher: username};
+        } else {
+            route = 'videos/play';
+            params = {id: video.id};
+        }
 
+
+        this.router.navigate([route], {queryParams: params});
     }
 
 }

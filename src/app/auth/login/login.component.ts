6f80ec8fe9465faa92656f5b8e1d33e5e39314d3
David Burton <davitburton280@gmail.com>
2020-11-12 15:27:29 +0400

    Fixed redirect-to-previous-url-after-login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -5,52 +5,54 @@ import {AuthService} from '@core/services/auth.service';
 import {Subscription} from 'rxjs';
 import {patternValidator} from '@core/helpers/pattern-validator';
 import {EMAIL_PATTERN} from '@core/constants/global';
+import {AuthGuard} from '@core/guards/auth.guard';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  loginForm: FormGroup;
-  subscriptions: Subscription[] = [];
-  isSubmitted = false;
+    loginForm: FormGroup;
+    subscriptions: Subscription[] = [];
+    isSubmitted = false;
 
-  constructor(
-    public router: Router,
-    private fb: FormBuilder,
-    private auth: AuthService
-  ) {
-    this.loginForm = this.fb.group({
-      email: ['', [Validators.required, patternValidator(EMAIL_PATTERN)]],
-      password: ['', Validators.required],
-    });
+    constructor(
+        public router: Router,
+        private fb: FormBuilder,
+        private auth: AuthService,
+        private authGuard: AuthGuard
+    ) {
+        this.loginForm = this.fb.group({
+            email: ['', [Validators.required, patternValidator(EMAIL_PATTERN)]],
+            password: ['', Validators.required],
+        });
 
-  }
-
-  ngOnInit(): void {
-  }
-
-  login() {
-    this.isSubmitted = true;
-    if (this.loginForm.valid) {
-      this.subscriptions.push(this.auth.login(this.loginForm.value).subscribe(async (dt: any) => {
-        localStorage.setItem('token', (dt.hasOwnProperty('token') ? dt.token : ''));
-        await this.router.navigate(['/']);
-      }));
     }
-  }
 
-  get email(): AbstractControl {
-    return this.loginForm.get('email');
-  }
+    ngOnInit(): void {
+    }
 
-  get pass(): AbstractControl {
-    return this.loginForm.get('password');
-  }
+    login() {
+        this.isSubmitted = true;
+        if (this.loginForm.valid) {
+            this.subscriptions.push(this.auth.login(this.loginForm.value).subscribe(async (dt: any) => {
+                localStorage.setItem('token', (dt.hasOwnProperty('token') ? dt.token : ''));
+                await this.router.navigate([this.authGuard.redirectUrl ? this.authGuard.redirectUrl : '/']);
+            }));
+        }
+    }
 
-  ngOnDestroy() {
-    this.subscriptions.forEach(s => s.unsubscribe());
-  }
+    get email(): AbstractControl {
+        return this.loginForm.get('email');
+    }
+
+    get pass(): AbstractControl {
+        return this.loginForm.get('password');
+    }
+
+    ngOnDestroy() {
+        this.subscriptions.forEach(s => s.unsubscribe());
+    }
 
 }

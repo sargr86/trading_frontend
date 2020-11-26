93f30258a604459a8f5aad91d80083a912f17873
David Burton <davitburton280@gmail.com>
2020-11-26 12:22:04 +0400

    Code verification look changes for user registration

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -7,16 +7,18 @@ import {RegisterComponent} from './register/register.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
 import { VerifyEmailComponent } from '@core/components/modals/verify-email/verify-email.component';
+import {MaterialModule} from '@core/modules/material.module';
 
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent, VerifyEmailComponent],
-  imports: [
-    CommonModule,
-    AuthRoutingModule,
-    ReactiveFormsModule,
-    BsDatepickerModule.forRoot()
-  ]
+    imports: [
+        CommonModule,
+        AuthRoutingModule,
+        ReactiveFormsModule,
+        BsDatepickerModule.forRoot(),
+        MaterialModule
+    ]
 })
 export class AuthModule {
 }

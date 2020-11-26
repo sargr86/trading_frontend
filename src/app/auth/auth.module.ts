deaae5ae2214cc4b3fd7df0dcebdee4b0aaade74
David Burton <davitburton280@gmail.com>
2020-11-26 15:46:59 +0400

    Fixed verification code sending, added forgot-password blank component, fixed minor bugs in play-video component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -8,10 +8,11 @@ import {ReactiveFormsModule} from '@angular/forms';
 import {BsDatepickerModule} from 'ngx-bootstrap/datepicker';
 import { VerifyEmailComponent } from '@core/components/modals/verify-email/verify-email.component';
 import {MaterialModule} from '@core/modules/material.module';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
 
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent, VerifyEmailComponent],
+  declarations: [LoginComponent, RegisterComponent, VerifyEmailComponent, ForgotPasswordComponent],
     imports: [
         CommonModule,
         AuthRoutingModule,

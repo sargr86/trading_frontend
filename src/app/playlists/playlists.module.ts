78cdcf9dbffa8e5e1db7ed464666152216571eeb
Davit Burton <davitburton280@gmail.com>
2020-12-08 16:53:21 +0400

    Added playlists info getting

--- src/app/playlists/playlists.module.ts
+++ src/app/playlists/playlists.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { PlaylistsRoutingModule } from './playlists-routing.module';
-import { SinglePlaylistComponent } from './single-playlist/single-playlist.component';
+import {PlaylistsRoutingModule} from './playlists-routing.module';
+import {SinglePlaylistComponent} from './single-playlist/single-playlist.component';
+import {SharedModule} from '@shared/shared.module';
 
 
 @NgModule({
-  declarations: [SinglePlaylistComponent],
-  imports: [
-    CommonModule,
-    PlaylistsRoutingModule
-  ]
+    declarations: [SinglePlaylistComponent],
+    imports: [
+        CommonModule,
+        PlaylistsRoutingModule,
+        SharedModule
+    ]
 })
-export class PlaylistsModule { }
+export class PlaylistsModule {
+}

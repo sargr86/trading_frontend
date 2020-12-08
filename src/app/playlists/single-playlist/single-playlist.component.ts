b16ba83be9866b3ec6333a2e9d0ea8bfff6db134
Davit Burton <davitburton280@gmail.com>
2020-12-08 12:23:48 +0400

    Single playlist getting test

--- src/app/playlists/single-playlist/single-playlist.component.ts
+++ src/app/playlists/single-playlist/single-playlist.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {PlaylistsService} from '@core/services/playlists.service';
 
 @Component({
-  selector: 'app-single-playlist',
-  templateUrl: './single-playlist.component.html',
-  styleUrls: ['./single-playlist.component.scss']
+    selector: 'app-single-playlist',
+    templateUrl: './single-playlist.component.html',
+    styleUrls: ['./single-playlist.component.scss']
 })
 export class SinglePlaylistComponent implements OnInit {
+    playlist;
 
-  constructor() { }
+    constructor(
+        public router: Router,
+        private route: ActivatedRoute,
+        private playlistsService: PlaylistsService
+    ) {
+        console.log(this.route.snapshot);
 
-  ngOnInit(): void {
-  }
+        const playlistId = this.route.snapshot?.params?.id;
+
+        if (playlistId) {
+            this.playlistsService.getById({id: playlistId}).subscribe(dt => {
+                this.playlist = dt;
+            });
+        }
+    }
+
+    ngOnInit(): void {
+    }
 
 }

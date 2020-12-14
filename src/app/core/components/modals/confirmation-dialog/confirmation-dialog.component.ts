8f7d06370651dd6678fde12fbc8584ea73bcb0b6
Davit Burton <davitburton280@gmail.com>
2020-12-14 16:16:48 +0400

    Added confirmation for removing video from playlist

---
+++ src/app/core/components/modals/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {MatDialogRef} from '@angular/material/dialog';
+
+@Component({
+    selector: 'app-confirmation-dialog',
+    templateUrl: './confirmation-dialog.component.html',
+    styleUrls: ['./confirmation-dialog.component.scss']
+})
+export class ConfirmationDialogComponent implements OnInit {
+
+    constructor(
+        private dialogRef: MatDialogRef<ConfirmationDialogComponent>
+    ) {
+    }
+
+    ngOnInit(): void {
+    }
+
+    confirm() {
+        this.dialogRef.close(true);
+    }
+    
+    cancel() {
+        this.dialogRef.close(false);
+    }
+
+}
--- src/app/playlists/single-playlist/single-playlist.component.ts
+++ src/app/playlists/single-playlist/single-playlist.component.ts
@@ -5,6 +5,7 @@ import {MatDialog} from '@angular/material/dialog';
 import {AddVideoToPlaylistDialogComponent} from '@core/components/modals/add-video-to-playlist-dialog/add-video-to-playlist-dialog.component';
 import {API_URL} from '@core/constants/global';
 import {moveItemInArray} from '@core/helpers/move-item-in-array';
+import {ConfirmationDialogComponent} from '@core/components/modals/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
     selector: 'app-single-playlist',
@@ -78,8 +79,15 @@ export class SinglePlaylistComponent implements OnInit {
     }
 
     removeVideoFromPlaylist(playlistId, videoId) {
-        this.playlistsService.removeVideoFromPlaylist({playlist_id: playlistId, video_id: videoId}).subscribe(dt => {
-            this.playlist = dt;
+        this.dialog.open(ConfirmationDialogComponent).afterClosed().subscribe(confirmed => {
+            if (confirmed) {
+                this.playlistsService.removeVideoFromPlaylist({
+                    playlist_id: playlistId,
+                    video_id: videoId
+                }).subscribe(dt => {
+                    this.playlist = dt;
+                });
+            }
         });
     }
 

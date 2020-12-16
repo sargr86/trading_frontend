f2087d34e81d2351e24f23477d4cab5d0e4c7531
Davit Burton <davitburton280@gmail.com>
2020-12-16 18:19:26 +0400

    Fixed minor issues

--- src/app/channels/show-channel/videos-tab/videos-tab.component.ts
+++ src/app/channels/show-channel/videos-tab/videos-tab.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {OwlOptions} from 'ngx-owl-carousel-o';
 import {API_URL, OWL_OPTIONS} from '@core/constants/global';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-videos-tab',
@@ -15,10 +16,28 @@ export class VideosTabComponent implements OnInit {
     @Input('channelUser') channelUser;
     @Input('authUser') authUser;
 
-    constructor() {
+    constructor(
+        public router: Router
+    ) {
     }
 
     ngOnInit(): void {
+    }
+
+    openVideoPage(video, username) {
+        console.log(username);
+        let route;
+        let params;
+        if (video.status === 'live') {
+            route = 'user/video/watch';
+            params = {session: video.session_name, publisher: username};
+        } else {
+            route = 'videos/play';
+            params = {id: video.id};
+        }
+
+
+        this.router.navigate([route], {queryParams: params});
     }
 
 }

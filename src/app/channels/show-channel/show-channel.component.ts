8fa8415b75f7d93bbbe2416475a3f96dbdf04bbb
Davit Burton <davitburton280@gmail.com>
2020-12-16 18:19:26 +0400

    Fixed minor issues

--- src/app/channels/show-channel/show-channel.component.ts
+++ src/app/channels/show-channel/show-channel.component.ts
@@ -82,21 +82,7 @@ export class ShowChannelComponent implements OnInit {
     }
 
 
-    openVideoPage(video, username) {
-        console.log(username);
-        let route;
-        let params;
-        if (video.status === 'live') {
-            route = 'user/video/watch';
-            params = {session: video.session_name, publisher: username};
-        } else {
-            route = 'videos/play';
-            params = {id: video.id};
-        }
 
-
-        this.router.navigate([route], {queryParams: params});
-    }
 
 
     searchVideos() {
--- src/app/channels/show-channel/watchlist-tab/watchlist-tab.component.ts
+++ src/app/channels/show-channel/watchlist-tab/watchlist-tab.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {VideoService} from '@core/services/video.service';
 import {OwlOptions} from 'ngx-owl-carousel-o';
 import {API_URL, OWL_OPTIONS} from '@core/constants/global';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-watchlist-tab',
@@ -14,7 +15,8 @@ export class WatchlistTabComponent implements OnInit {
     apiUrl = API_URL;
 
     constructor(
-        private videoService: VideoService
+        private videoService: VideoService,
+        public router: Router
     ) {
     }
 
@@ -22,6 +24,22 @@ export class WatchlistTabComponent implements OnInit {
         this.videoService.getVideosByAuthor({}).subscribe(dt => {
             this.watchlistVideos = dt;
         });
+    }
+
+    openVideoPage(video, username) {
+        console.log(username);
+        let route;
+        let params;
+        if (video.status === 'live') {
+            route = 'user/video/watch';
+            params = {session: video.session_name, publisher: username};
+        } else {
+            route = 'videos/play';
+            params = {id: video.id};
+        }
+
+
+        this.router.navigate([route], {queryParams: params});
     }
 
 }

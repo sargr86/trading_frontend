e289b0bfcfbef50c06bfc2bb30ebdd3f262be7fd
Davit Burton <davitburton280@gmail.com>
2020-12-16 17:09:15 +0400

    Single-playlist component separated into thematic subcomponents

--- src/app/playlists/single-playlist/single-playlist.component.ts
+++ src/app/playlists/single-playlist/single-playlist.component.ts
@@ -17,111 +17,34 @@ import {AddVideoToAnotherPlaylistComponent} from '@core/components/modals/add-vi
 export class SinglePlaylistComponent implements OnInit {
     playlist;
     apiUrl = API_URL;
-    playlistInfoForm: FormGroup;
-    editMode = false;
 
     constructor(
         public router: Router,
         private route: ActivatedRoute,
         private playlistsService: PlaylistsService,
         private dialog: MatDialog,
-        private fb: FormBuilder
     ) {
-        const playlistId = this.route.snapshot?.params?.id;
 
-        if (playlistId) {
-            this.playlistInfoForm = this.fb.group({
-                id: [''],
-                name: ['', Validators.required],
-                description: ['']
-            });
-            this.getPlaylistDetails(playlistId);
-
-        }
     }
 
     ngOnInit(): void {
+        this.getPlaylistDetails();
     }
 
-    openVideosModal() {
-        this.dialog.open(AddVideoToPlaylistDialogComponent, {data: {playlist: this.playlist}}).afterClosed().subscribe(dt => {
-            this.getPlaylistDetails(this.playlist.id);
-        });
-    }
 
-    updatePrivacy(value, playlist) {
-        this.playlistsService.updatePrivacy({privacy: value, id: playlist.id}).subscribe(dt => {
 
-        });
 
-    }
+    getPlaylistDetails() {
 
-    openVideoPage(video) {
-        this.router.navigate(['videos/play'], {queryParams: {id: video.id}});
-    }
+        const playlistId = this.route.snapshot?.params?.id;
 
-    getPlaylistDetails(playlistId) {
-        this.playlistsService.getById({playlist_id: playlistId}).subscribe(dt => {
-            this.playlist = dt;
-        });
-    }
-
-    dragDropped(e, video) {
-        console.log(e)
-        // console.log(channel)
-        this.playlist.videos = moveItemInArray(this.playlist.videos, e.previousIndex, e.currentIndex);
-        // console.log(this.playlist)
-        const sendData = {
-            rows: JSON.stringify(this.playlist),
-            playlist_id: this.playlist.id,
-            // user_id: this.authUser.id
-        };
-        this.playlistsService.updateVideoPosition(sendData).subscribe(dt => {
-        });
-    }
-
-    changePlaylistThumbnail(playlistId, thumbnail) {
-        const params = {playlist_id: playlistId, ...{thumbnail}};
-        this.playlistsService.changePlaylistThumbnail(params).subscribe(dt => {
-            this.playlist = dt;
-        });
-    }
-
-    removeVideoFromPlaylist(playlistId, videoId) {
-        this.dialog.open(ConfirmationDialogComponent).afterClosed().subscribe(confirmed => {
-            if (confirmed) {
-                this.playlistsService.removeVideoFromPlaylist({
-                    playlist_id: playlistId,
-                    video_id: videoId
-                }).subscribe(dt => {
-                    this.playlist = dt;
-                });
-            }
-        });
-    }
-
-    editPlaylistInfo(playlist) {
-        this.editMode = true;
-        this.playlistInfoForm.patchValue(playlist);
-    }
-
-    savePlaylistInfoChanges() {
-        this.playlistsService.updatePlaylistInfo(this.playlistInfoForm.value).subscribe((dt) => {
-            this.editMode = false;
-            this.playlist = dt;
-        });
-        console.log(this.playlistInfoForm.value)
-    }
-
-    addToAnotherPlaylist(video) {
-        this.playlistsService.get().subscribe(dt => {
-            this.dialog.open(AddVideoToAnotherPlaylistComponent, {
-                width: '500px',
-                data: {video_id: video.id, playlists: dt}
-            }).afterClosed().subscribe(result => {
-                this.getPlaylistDetails(this.playlist.id);
+        if (playlistId) {
+            this.playlistsService.getById({playlist_id: playlistId}).subscribe(dt => {
+                this.playlist = dt;
             });
-        });
+        }
     }
+
+
 
 }

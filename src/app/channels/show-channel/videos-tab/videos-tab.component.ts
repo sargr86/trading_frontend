ae49aa35e1af048d392b542677ab59cb01a6504b
David Burton <davitburton280@gmail.com>
2021-02-02 11:47:08 +0400

    Added filters for videos tab in user channel page

--- src/app/channels/show-channel/videos-tab/videos-tab.component.ts
+++ src/app/channels/show-channel/videos-tab/videos-tab.component.ts
@@ -3,6 +3,7 @@ import {OwlOptions} from 'ngx-owl-carousel-o';
 import {API_URL, OWL_OPTIONS} from '@core/constants/global';
 import {Router} from '@angular/router';
 import {VideoService} from '@core/services/video.service';
+import {SubjectService} from '@core/services/subject.service';
 
 @Component({
     selector: 'app-videos-tab',
@@ -13,21 +14,36 @@ export class VideosTabComponent implements OnInit {
 
     owlOptions: OwlOptions = OWL_OPTIONS;
     apiUrl = API_URL;
+    showFilters = false;
 
     @Input('channelUser') channelUser;
     @Input('authUser') authUser;
 
     constructor(
         public router: Router,
-        private videoService: VideoService
+        private videoService: VideoService,
+        private subjectService: SubjectService
     ) {
     }
 
     ngOnInit(): void {
+
+        this.subjectService.getToggleFiltersData().subscribe(dt => {
+            this.showFilters = dt;
+        });
     }
 
     getSearchResults(dt) {
         this.channelUser.videos = dt;
+    }
+
+    getFilteredVideos(e) {
+        this.videoService.searchInUserVideos({
+            user_id: this.channelUser.id,
+            filters: JSON.stringify(e)
+        }).subscribe(dt => {
+            this.channelUser.videos = dt?.videos;
+        });
     }
 
 }

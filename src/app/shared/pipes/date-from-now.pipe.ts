e922fbb0d5c3a3e57ff596cadcd8c20c1cf80909
Davit Burton <davitburton280@gmail.com>
2021-02-04 12:27:58 +0400

    1 day ago

--- src/app/shared/pipes/date-from-now.pipe.ts
+++ src/app/shared/pipes/date-from-now.pipe.ts
@@ -1,13 +1,17 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 import * as moment from 'moment';
 
 @Pipe({
-  name: 'dateFromNow'
+    name: 'dateFromNow'
 })
 export class DateFromNowPipe implements PipeTransform {
 
-  transform(datetime): unknown {
-      return moment(datetime).fromNow();
-  }
+    transform(datetime): unknown {
+        let fromNow = moment(datetime).fromNow();
+        if (fromNow === 'a day ago') {
+            fromNow = '1 day ago';
+        }
+        return fromNow;
+    }
 
 }

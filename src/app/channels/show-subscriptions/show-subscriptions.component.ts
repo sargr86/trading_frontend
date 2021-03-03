e37ea1941dcdfff3729b490297525914ad916cf1
Davit Burton <davitburton280@gmail.com>
2021-03-03 15:53:00 +0400

    Recent bug fixes for channel subscriptions

--- src/app/channels/show-subscriptions/show-subscriptions.component.ts
+++ src/app/channels/show-subscriptions/show-subscriptions.component.ts
@@ -4,6 +4,8 @@ import {GetAuthUserPipe} from '@shared/pipes/get-auth-user.pipe';
 import {AuthService} from '@core/services/auth.service';
 import {API_URL} from '@core/constants/global';
 import * as moment from 'moment';
+import {SubjectService} from '@core/services/subject.service';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-show-subscriptions',
@@ -19,13 +21,39 @@ export class ShowSubscriptionsComponent implements OnInit {
         private channelsService: ChannelsService,
         private getAuthUser: GetAuthUserPipe,
         public auth: AuthService,
+        public router: Router,
+        private subject: SubjectService
     ) {
         this.authUser = this.getAuthUser.transform();
     }
 
     ngOnInit(): void {
+        this.getUserChannelSubscriptions();
+    }
+
+    openChannelPage(channel, username) {
+        console.log(channel)
+        this.router.navigate(['channels/show'], {queryParams: {username}});
+    }
+
+    getUserChannelSubscriptions() {
         this.channelsService.getSubscriptions({user_id: this.authUser.id}).subscribe(dt => {
             this.userChannels = dt;
+        });
+    }
+
+
+    checkIfSubscribed(channel) {
+        return channel.channel_subscribers.subscriber_id === this.authUser?.id;
+    }
+
+    subscribeToChannel(channel) {
+        this.channelsService.subscribeToChannel({user_id: this.authUser.id, channel_id: channel.id}).subscribe(dt => {
+            // this.subscribedToChannel = dt.status === 'Subscribed';
+            this.channelsService.getUserChannelSubscriptions({user_id: this.authUser.id}).subscribe(d => {
+                this.subject.setUserSubscriptions(d);
+                this.getUserChannelSubscriptions();
+            });
         });
     }
 

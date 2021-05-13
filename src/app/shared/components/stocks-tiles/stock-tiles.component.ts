38a0df62d5bf914897f2b9863ea062323ad6f8df
David Burton <davitburton280@gmail.com>
2021-05-13 15:17:02 +0400

    Stocks profile wth new stocks-list reusable component

--- src/app/shared/components/stocks-tiles/stock-tiles.component.ts
+++ src/app/shared/components/stocks-tiles/stock-tiles.component.ts
@@ -5,6 +5,7 @@ import {STOCK_TILE_CHART_SETTINGS} from '@core/constants/charts';
 import {UpdateUserStocksPipe} from '@shared/pipes/update-user-stocks.pipe';
 import {GetAuthUserPipe} from '@shared/pipes/get-auth-user.pipe';
 import {CdkDragDrop} from '@angular/cdk/drag-drop';
+import {Router} from '@angular/router';
 
 @Component({
     selector: 'app-stock-tiles',
@@ -21,6 +22,8 @@ export class StockTilesComponent implements OnInit {
     @Input('type') selectedStockType: Stock | null = null;
     @Input('allStocksList') allStocksList = false;
     @Input('dragDropDisabled') dragDropDisabled = false;
+    @Input('followingAllowed') followingAllowed = true;
+    @Input('stockProfileAllowed') stockProfileAllowed = false;
     @Output('updatedStocksList') updatedStocksList = new EventEmitter();
     @Output('updatedStocksPriority') updatedStocksPriority = new EventEmitter();
 
@@ -28,6 +31,7 @@ export class StockTilesComponent implements OnInit {
         private subject: SubjectService,
         private updateStocks: UpdateUserStocksPipe,
         private getAuthUser: GetAuthUserPipe,
+        public router: Router
     ) {
     }
 
@@ -54,5 +58,13 @@ export class StockTilesComponent implements OnInit {
         this.passedStocks[event.container.data.index] = event.previousContainer.data.item;
 
         this.updatedStocksPriority.emit(this.passedStocks);
+    }
+
+    openStockProfile(stock) {
+        if (this.stockProfileAllowed) {
+            this.router.navigateByUrl('/test', {skipLocationChange: true}).then(async () =>
+                await this.router.navigate([`stocks/${stock}/analytics`])
+            );
+        }
     }
 }

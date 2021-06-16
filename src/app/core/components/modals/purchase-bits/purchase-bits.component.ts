7521665a3e327d948a11dd9c3c81315d62ff4525
David Burton <davitburton280@gmail.com>
2021-06-16 14:13:38 +0400

    Opening complete purchase modal from purchase-bits

--- src/app/core/components/modals/purchase-bits/purchase-bits.component.ts
+++ src/app/core/components/modals/purchase-bits/purchase-bits.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import {CompletePurchaseModalComponent} from '@shared/components/complete-purchase-modal/complete-purchase-modal.component';
 
 @Component({
-  selector: 'app-purchase-bits',
-  templateUrl: './purchase-bits.component.html',
-  styleUrls: ['./purchase-bits.component.scss']
+    selector: 'app-purchase-bits',
+    templateUrl: './purchase-bits.component.html',
+    styleUrls: ['./purchase-bits.component.scss']
 })
 export class PurchaseBitsComponent implements OnInit {
 
-  constructor() { }
+    bitPurchases = [
+        {bitPrice: 300, discount: 29, currencyPrice: 3},
+        {bitPrice: 300, discount: 30, currencyPrice: 3},
+        {bitPrice: 300, discount: 2, currencyPrice: 5},
+        {bitPrice: 300, discount: 29, currencyPrice: 3},
+        {bitPrice: 300, discount: 29, currencyPrice: 3},
+        {bitPrice: 300, discount: 29, currencyPrice: 308},
+    ];
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private dialog: MatDialog
+    ) {
+    }
+
+    ngOnInit(): void {
+    }
+
+    openPurchaseModal(purchase) {
+        this.dialog.open(CompletePurchaseModalComponent, {data: purchase}).afterClosed().subscribe(dt => {
+
+        });
+    }
 
 }
--- src/app/shared/components/complete-purchase-modal/complete-purchase-modal.component.ts
+++ src/app/shared/components/complete-purchase-modal/complete-purchase-modal.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-complete-purchase-modal',
-  templateUrl: './complete-purchase-modal.component.html',
-  styleUrls: ['./complete-purchase-modal.component.scss']
+    selector: 'app-complete-purchase-modal',
+    templateUrl: './complete-purchase-modal.component.html',
+    styleUrls: ['./complete-purchase-modal.component.scss']
 })
 export class CompletePurchaseModalComponent implements OnInit {
+    purchase;
+    currentDate = new Date();
+    constructor(
+        @Inject(MAT_DIALOG_DATA) public data: any,
+        private matDialogRef: MatDialogRef<CompletePurchaseModalComponent>
+    ) {
+        this.purchase = data;
+    }
 
-  constructor() { }
+    ngOnInit(): void {
+    }
 
-  ngOnInit(): void {
-  }
+    closeModal(){
+        this.matDialogRef.close();
+    }
 
 }

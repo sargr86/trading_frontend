6caaa43fa8b90d6461ac44ffa8b00503e75d9cbc
Davit Burton <davitburton280@gmail.com>
2021-07-27 11:01:32 +0400

    Separated wallet tables column content getting to a pipe

---
+++ src/app/shared/pipes/get-wallet-tables-column-contents.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { GetWalletTablesColumnContentsPipe } from './get-wallet-tables-column-contents.pipe';
+
+describe('GetWalletTablesColumnContentsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new GetWalletTablesColumnContentsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});

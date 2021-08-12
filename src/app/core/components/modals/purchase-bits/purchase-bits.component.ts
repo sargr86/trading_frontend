a67d5e5d09b082030f93721aecb28b72ea910d30
Clever Coder <clevercoder8@gmail.com>
2021-08-12 16:40:40 +0400

    Recent payment fixes

--- src/app/core/components/modals/purchase-bits/purchase-bits.component.ts
+++ src/app/core/components/modals/purchase-bits/purchase-bits.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import {CompletePurchaseModalComponent} from '@shared/components/complete-purchase-modal/complete-purchase-modal.component';
 import {GetAuthUserPipe} from '@shared/pipes/get-auth-user.pipe';
 import {SubjectService} from '@core/services/subject.service';
 import {ProductsService} from '@core/services/wallet/products.service';
+import {PaymentsService} from '@core/services/wallet/payments.service';
 
 @Component({
     selector: 'app-purchase-bits',
@@ -17,9 +18,12 @@ export class PurchaseBitsComponent implements OnInit {
     coinImages = ['gold', 'silver', 'pink', 'green', 'blue'];
     authUser;
 
+    @Input() totals;
+
     constructor(
         private dialog: MatDialog,
         private productsService: ProductsService,
+        private paymentsService: PaymentsService,
         private getAuthUser: GetAuthUserPipe,
         private subject: SubjectService
     ) {
@@ -30,15 +34,34 @@ export class PurchaseBitsComponent implements OnInit {
         this.productsService.getStripeProducts().subscribe(dt => {
             this.bitProducts = dt;
         });
+
+        this.subject.getPurchasedBitsData().subscribe(dt => {
+            // console.log(dt)
+        });
     }
 
     openPurchaseModal(purchase) {
         this.dialog.open(CompletePurchaseModalComponent, {
             data: purchase,
             width: '800px'
-        }).afterClosed().subscribe(dt => {
-            this.subject.setPurchasedBitsData(dt);
+        }).afterClosed().subscribe((dt) => {
+            this.totals = {purchased: {coins: 0, dollars: 0}, transferred: {coins: 0, dollars: 0}};
+            if (dt) {
+                this.paymentsService.getPurchasesHistory(dt).subscribe(ph => {
+                    // this.subject.setPurchasedBitsData(d);
+                    this.countTotals(ph.filter(d => d.transfer_group === 'purchases'), 'purchased');
+                });
+            }
         });
+    }
+
+    countTotals(dt, key) {
+        dt.map(d => {
+            this.totals[key].coins += (d.amount / 0.0199) / 100;
+            this.totals[key].dollars += d.amount / 100;
+        });
+
+
     }
 
     createArray(len) {

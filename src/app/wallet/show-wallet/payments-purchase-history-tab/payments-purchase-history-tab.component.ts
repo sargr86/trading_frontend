3bdbd13e17d016b87c207edf1031c493c55d9f52
Clever Coder <clevercoder8@gmail.com>
2021-08-17 16:18:10 +0400

    Added sorting for purchases table

--- src/app/wallet/show-wallet/payments-purchase-history-tab/payments-purchase-history-tab.component.ts
+++ src/app/wallet/show-wallet/payments-purchase-history-tab/payments-purchase-history-tab.component.ts
@@ -1,18 +1,18 @@
-import {Component, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
-import {CurrencyPipe, DatePipe} from '@angular/common';
+import {AfterViewInit, Component, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatPaginator} from '@angular/material/paginator';
 import {Subscription} from 'rxjs';
-import {CapitalizeAddSpacesPipe} from '@shared/pipes/capitalize-add-spaces.pipe';
 import {SubjectService} from '@core/services/subject.service';
 import {PaymentsService} from '@core/services/wallet/payments.service';
+import {MatSort, Sort} from '@angular/material/sort';
+import {sortTableData} from '@core/helpers/sort-table-data-by-column';
 
 @Component({
     selector: 'app-payments-purchase-history-tab',
     templateUrl: './payments-purchase-history-tab.component.html',
     styleUrls: ['./payments-purchase-history-tab.component.scss']
 })
-export class PaymentsPurchaseHistoryTabComponent implements OnInit, OnDestroy {
+export class PaymentsPurchaseHistoryTabComponent implements OnInit, AfterViewInit, OnDestroy {
     subscriptions: Subscription[] = [];
     purchases = [];
     filteredPurchases = [];
@@ -21,6 +21,7 @@ export class PaymentsPurchaseHistoryTabComponent implements OnInit, OnDestroy {
 
     @Input() userCards = [];
     @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatSort) sort: MatSort;
 
     constructor(
         private paymentsService: PaymentsService,
@@ -46,10 +47,21 @@ export class PaymentsPurchaseHistoryTabComponent implements OnInit, OnDestroy {
             this.subscriptions.push(this.paymentsService.getPurchasesHistory(params).subscribe(dt => {
                 this.purchases = dt;
                 this.filteredPurchases = dt;
-                this.tableData = new MatTableDataSource(dt);
+                this.tableData = new MatTableDataSource(this.filteredPurchases);
                 this.tableData.paginator = this.paginator;
+                this.tableData.sort = this.sort;
             }));
         }
+    }
+
+    ngAfterViewInit() {
+        this.sort.sortChange.subscribe((sort: Sort) => {
+            this.filteredPurchases = sortTableData(this.filteredPurchases, 'created', sort.direction);
+            this.paginator.pageIndex = 0;
+            this.tableData = new MatTableDataSource(this.filteredPurchases);
+            this.tableData.paginator = this.paginator;
+        });
+
     }
 
     ngOnDestroy(): void {

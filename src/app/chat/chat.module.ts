fb2bde72d8c3ccdfba2d6ae7a902736a7c797837
Clever Coder <clevercoder8@gmail.com>
2021-09-09 17:12:11 +0400

    General chat messages - first look

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { ChatRoutingModule } from './chat-routing.module';
-import { ShowChatroomsComponent } from './show-chatrooms/show-chatrooms.component';
-import { ShowMessagesComponent } from './show-messages/show-messages.component';
+import {ChatRoutingModule} from './chat-routing.module';
+import {ShowChatroomsComponent} from './show-chatrooms/show-chatrooms.component';
+import {ShowMessagesComponent} from './show-messages/show-messages.component';
+import {SharedModule} from '@shared/shared.module';
 
 
 @NgModule({
-  declarations: [ShowChatroomsComponent, ShowMessagesComponent],
-  imports: [
-    CommonModule,
-    ChatRoutingModule
-  ]
+    declarations: [ShowChatroomsComponent, ShowMessagesComponent],
+    imports: [
+        CommonModule,
+        ChatRoutingModule,
+        SharedModule
+    ]
 })
-export class ChatModule { }
+export class ChatModule {
+}
--- src/app/chat/show-messages/show-messages.component.ts
+++ src/app/chat/show-messages/show-messages.component.ts
@@ -1,4 +1,6 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {ChatService} from '@core/services/chat.service';
+import {GetAuthUserPipe} from '@shared/pipes/get-auth-user.pipe';
 
 @Component({
     selector: 'app-show-messages',
@@ -7,15 +9,39 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ShowMessagesComponent implements OnInit {
     activeTab = 'direct';
+    authUser;
+    usersMessages = [];
+    selectedUserMessages = {messages: [], user: {}};
+    activeUser;
 
-    constructor() {
+    constructor(
+        private chatService: ChatService,
+        private getAuthUser: GetAuthUserPipe,
+        private cdr: ChangeDetectorRef
+    ) {
     }
 
     ngOnInit(): void {
+        this.authUser = this.getAuthUser.transform();
+        this.getUsersMessages();
     }
 
     changeTab(tab) {
         this.activeTab = tab;
+    }
+
+    getUsersMessages() {
+        this.chatService.getGeneralChatMessages({from_id: this.authUser.id, to_id: '', personal: 1}).subscribe(dt => {
+            this.usersMessages = dt;
+            this.activeUser = dt[0].user;
+            this.selectedUserMessages = this.usersMessages.find(m => m.user.id === this.activeUser.id);
+            console.log(this.selectedUserMessages)
+        });
+    }
+
+    makeUserActive(user) {
+        this.activeUser = user;
+        this.selectedUserMessages = this.usersMessages.find(m => m.user.id === user.id);
     }
 
 }

43beb398827d88a31bc16e2195b64a39cf663bd8
liamw6367@gmail.com <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>
2021-12-01 11:23:21 +0400

    First preparations for connections page

--- src/app/user/show-connections/connections-tab/connections-tab.component.ts
+++ src/app/user/show-connections/connections-tab/connections-tab.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {UsersService} from "@core/services/users.service";
 
 @Component({
-  selector: 'app-connections-tab',
-  templateUrl: './connections-tab.component.html',
-  styleUrls: ['./connections-tab.component.scss']
+    selector: 'app-connections-tab',
+    templateUrl: './connections-tab.component.html',
+    styleUrls: ['./connections-tab.component.scss']
 })
 export class ConnectionsTabComponent implements OnInit {
+    @Input() authUser;
+    connections = [];
 
-  constructor() { }
+    constructor(
+        private usersService: UsersService
+    ) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.getConnections();
+    }
+
+    getConnections() {
+        this.usersService.getUserContacts({user_id: this.authUser.id}).subscribe(dt => {
+            this.connections = dt;
+        });
+    }
 
 }
--- src/app/user/show-connections/show-connections.component.ts
+++ src/app/user/show-connections/show-connections.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Subscription} from 'rxjs';
+import {GetAuthUserPipe} from "@shared/pipes/get-auth-user.pipe";
 
 @Component({
     selector: 'app-show-connections',
@@ -9,11 +10,15 @@ import {Subscription} from 'rxjs';
 export class ShowConnectionsComponent implements OnInit {
     selectedTabIndex = 0;
     subscriptions: Subscription[] = [];
+    authUser;
 
-    constructor() {
+    constructor(
+        private getAuthUser: GetAuthUserPipe
+    ) {
     }
 
     ngOnInit(): void {
+        this.authUser = this.getAuthUser.transform();
     }
 
     tabChange(e) {

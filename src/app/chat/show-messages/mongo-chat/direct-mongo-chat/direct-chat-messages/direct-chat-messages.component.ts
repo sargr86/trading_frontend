0ec3b45e52d935ef8da7a920838ba87a3fe1c608
Grigor Sargsian <sargr86@gmail.com>
2022-01-13 15:05:37 +0400

    Chat messages selected user row implemented

--- src/app/chat/show-messages/mongo-chat/direct-mongo-chat/direct-chat-messages/direct-chat-messages.component.ts
+++ src/app/chat/show-messages/mongo-chat/direct-mongo-chat/direct-chat-messages/direct-chat-messages.component.ts
@@ -1,23 +1,35 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {UserMessagesSubjectService} from '@core/services/user-messages-subject.service';
+import {MobileResponsiveHelper} from '@core/helpers/mobile-responsive-helper';
+import {Subscription} from 'rxjs';
 
 @Component({
     selector: 'app-direct-chat-messages',
     templateUrl: './direct-chat-messages.component.html',
     styleUrls: ['./direct-chat-messages.component.scss']
 })
-export class DirectChatMessagesComponent implements OnInit {
+export class DirectChatMessagesComponent implements OnInit, OnDestroy {
+    subscriptions: Subscription[] = [];
     selectedUserMessages;
 
     constructor(
         private userMessagesStore: UserMessagesSubjectService,
+        public mobileHelper: MobileResponsiveHelper,
     ) {
     }
 
     ngOnInit(): void {
-        this.userMessagesStore.selectedUserMessages$.subscribe(dt => {
+        this.subscriptions.push(this.userMessagesStore.selectedUserMessages$.subscribe(dt => {
             this.selectedUserMessages = dt;
-        });
+        }));
+    }
+
+    ifContactBlocked(user) {
+        return user.users_connections?.[0].is_blocked;
+    }
+
+    ngOnDestroy() {
+        this.subscriptions.forEach(s => s.unsubscribe());
     }
 
 }

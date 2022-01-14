e6096d86e7b4b51242e859025d12ebf993daa0cd
Grigor Sargsian <sargr86@gmail.com>
2022-01-14 10:16:14 +0400

    Loaded chat messages by users

--- src/app/chat/show-messages/mongo-chat/direct-mongo-chat/direct-chat-messages/direct-chat-messages.component.ts
+++ src/app/chat/show-messages/mongo-chat/direct-mongo-chat/direct-chat-messages/direct-chat-messages.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {UserMessagesSubjectService} from '@core/services/user-messages-subject.service';
 import {MobileResponsiveHelper} from '@core/helpers/mobile-responsive-helper';
 import {Subscription} from 'rxjs';
+import {GetElegantDatePipe} from '@shared/pipes/get-elegant-date.pipe';
+import {GroupByPipe} from '@shared/pipes/group-by.pipe';
 
 @Component({
     selector: 'app-direct-chat-messages',
@@ -9,23 +11,43 @@ import {Subscription} from 'rxjs';
     styleUrls: ['./direct-chat-messages.component.scss']
 })
 export class DirectChatMessagesComponent implements OnInit, OnDestroy {
+    @Input() authUser;
+    @Output() refresh = new EventEmitter();
     subscriptions: Subscription[] = [];
+
     selectedUserMessages;
 
     constructor(
         private userMessagesStore: UserMessagesSubjectService,
         public mobileHelper: MobileResponsiveHelper,
+        private getElegantDate: GetElegantDatePipe,
+        private groupByDate: GroupByPipe,
     ) {
     }
 
     ngOnInit(): void {
-        this.subscriptions.push(this.userMessagesStore.selectedUserMessages$.subscribe(dt => {
+        this.subscriptions.push(this.userMessagesStore.selectedUserMessages$.subscribe((dt: any) => {
             this.selectedUserMessages = dt;
         }));
     }
 
-    ifContactBlocked(user) {
+    getMessagesByDate(dt){
+        return this.groupByDate.transform(dt, 'created_at');
+    }
+
+    getSeenTooltip(message) {
+        const user = message.to_user;
+        const seenDate = this.getElegantDate.transform(message.seen_at);
+
+        return `${user.first_name} ${user.last_name} at ${seenDate}`;
+    }
+
+    isContactBlocked(user) {
         return user.users_connections?.[0].is_blocked;
+    }
+
+    isOwnMessage(from_id) {
+        return from_id === this.authUser.id;
     }
 
     ngOnDestroy() {

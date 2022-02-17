72823feae5c71c48253e4bfc197869c803705229
liamw6367@gmail.com <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>
2022-02-17 16:31:38 +0400

    Code clean up

--- src/app/core/components/layout/right-sidebar/right-sidebar.component.ts
+++ src/app/core/components/layout/right-sidebar/right-sidebar.component.ts
@@ -1,7 +1,5 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {GetAuthUserPipe} from '@shared/pipes/get-auth-user.pipe';
-import {ChatService} from '@core/services/chat.service';
-import {UsersMessagesSubjectService} from '@core/services/stores/users-messages-subject.service';
 
 @Component({
     selector: 'app-right-sidebar',
@@ -10,7 +8,7 @@ import {UsersMessagesSubjectService} from '@core/services/stores/users-messages-
 })
 export class RightSidebarComponent implements OnInit {
     @Input() shownItems = 'notifications';
-    @Output('closeSidenav') closeSidenav = new EventEmitter();
+    @Output() closeSidenav = new EventEmitter();
     @Output() openBottomChatBox = new EventEmitter();
     authUser;
     subscriptions = [];
@@ -18,28 +16,11 @@ export class RightSidebarComponent implements OnInit {
 
     constructor(
         private getAuthUser: GetAuthUserPipe,
-        private chatService: ChatService,
-        private usersMessagesStore: UsersMessagesSubjectService
     ) {
     }
 
     ngOnInit(): void {
         this.authUser = this.getAuthUser.transform();
-        if (this.authUser) {
-            // this.getUsersMessages();
-        }
-    }
-
-    getUsersMessages() {
-
-        this.subscriptions.push(this.chatService.getDirectMessages({
-            user_id: this.authUser.id,
-        }).subscribe(dt => {
-            this.usersMessages = dt;
-            this.usersMessagesStore.setUserMessages(dt);
-            console.log(this.usersMessagesStore.usersMessages)
-        }));
-
     }
 
 

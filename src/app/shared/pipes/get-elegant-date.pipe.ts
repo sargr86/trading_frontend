65859dd0d19a783024c42e9b95400e37226fca10
liamw6367@gmail.com <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>
2022-02-17 10:32:52 +0400

    Invalid date fix in notifications when just arrived one

--- src/app/shared/pipes/get-elegant-date.pipe.ts
+++ src/app/shared/pipes/get-elegant-date.pipe.ts
@@ -7,12 +7,11 @@ import * as moment from 'moment';
 export class GetElegantDatePipe implements PipeTransform {
 
     transform(date): string {
-        date = new Date(date);
+        date = date ? new Date(date) : new Date();
         const thisWeekDate = moment(date).isSame(new Date(), 'week');
         let result = moment(date).format(thisWeekDate ? 'ddd HH:mm' : 'MMM DD, YYYY HH:mm');
         const isToday = moment(date).isSame(Date.now(), 'day');
         const isYesterday = moment(date).isSame(moment().subtract(1, 'day'), 'day');
-
         if (isToday) {
             result = 'Today at ' + moment(date).format('HH:mm');
         } else if (isYesterday) {

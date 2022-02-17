4437d2fbdaa531dce443adbed13bbc2de12a9fbb
liamw6367@gmail.com <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>
2022-02-17 09:23:32 +0400

    New unseen messages counter in section links component

--- src/app/core/components/layout/left-sidebar/section-links/section-links.component.ts
+++ src/app/core/components/layout/left-sidebar/section-links/section-links.component.ts
@@ -1,4 +1,13 @@
-import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
+import {
+    AfterViewChecked,
+    ChangeDetectorRef,
+    Component,
+    EventEmitter,
+    Input,
+    OnDestroy,
+    OnInit,
+    Output
+} from '@angular/core';
 import {MAIN_SECTIONS} from '@core/constants/global';
 import {Router} from '@angular/router';
 import {environment} from '@env';
@@ -8,13 +17,15 @@ import {SocketIoService} from '@core/services/socket-io.service';
 import {ChatService} from '@core/services/chat.service';
 import {SubjectService} from '@core/services/subject.service';
 import {Subscription} from 'rxjs';
+import {UserMessagesSubjectService} from '@core/services/user-messages-subject.service';
+import {GroupsMessagesSubjectService} from '@core/services/stores/groups-messages-subject.service';
 
 @Component({
     selector: 'app-section-links',
     templateUrl: './section-links.component.html',
     styleUrls: ['./section-links.component.scss']
 })
-export class SectionLinksComponent implements OnInit, OnDestroy {
+export class SectionLinksComponent implements OnInit, OnDestroy, AfterViewChecked {
     mainSections = MAIN_SECTIONS;
     envName;
     newMessageSources = 0;
@@ -30,26 +41,30 @@ export class SectionLinksComponent implements OnInit, OnDestroy {
         public auth: AuthService,
         private socketService: SocketIoService,
         private subject: SubjectService,
-        private chatService: ChatService
+        private chatService: ChatService,
+        private usersMessagesStore: UserMessagesSubjectService,
+        private groupsMessagesStore: GroupsMessagesSubjectService,
+        private cdr: ChangeDetectorRef
     ) {
     }
 
     ngOnInit(): void {
         this.envName = environment.envName;
-        let directNewMessagesCount = 0;
-        let groupNewMessagesCount = 0;
-        this.subscriptions.push(this.subject.getNewMessagesSourceData().subscribe(data => {
-            // console.log('new messages from ' + data.type + ':' + this.newMessageSources)
-            if (data.type === 'direct') {
+        // let directNewMessagesCount = 0;
+        // let groupNewMessagesCount = 0;
+        // this.subscriptions.push(this.subject.getNewMessagesSourceData().subscribe(data => {
+        //     // console.log('new messages from ' + data.type + ':' + this.newMessageSources)
+        //     if (data.type === 'direct') {
+        //
+        //         directNewMessagesCount = data.sources;
+        //     } else {
+        //         groupNewMessagesCount = data.sources || 0;
+        //         // console.log(groupNewMessagesCount)
+        //     }
+        //     this.newMessageSources = directNewMessagesCount + groupNewMessagesCount;
+        //     // console.log('received:', directNewMessagesCount, groupNewMessagesCount)
+        // }));
 
-                directNewMessagesCount = data.sources;
-            } else {
-                groupNewMessagesCount = data.sources || 0;
-                // console.log(groupNewMessagesCount)
-            }
-            this.newMessageSources = directNewMessagesCount + groupNewMessagesCount;
-            // console.log('received:', directNewMessagesCount, groupNewMessagesCount)
-        }));
     }
 
 
@@ -59,6 +74,32 @@ export class SectionLinksComponent implements OnInit, OnDestroy {
             await this.router.navigate([route], {queryParams: params})
         );
     }
+
+    getUnreadMessagesCount() {
+
+        const directMessages = this.usersMessagesStore.userMessages
+            ?.filter(m => m.direct_messages
+                ?.filter(d => !d.seen && d.from_id !== this.authUser.id).length > 0).length;
+
+        const groupMessages = this.groupsMessagesStore.groupsMessages
+            ?.filter(m => {
+                return m.group_messages
+                    ?.find(message => {
+                        let found = false;
+                        if (message.from_id !== this.authUser.id) {
+                            found = !message.seen.find(sb => sb.seen_by.id === this.authUser.id);
+                        }
+                        return found;
+                    });
+            }).length;
+        return directMessages + groupMessages;
+    }
+
+    ngAfterViewChecked() {
+        this.newMessageSources = this.getUnreadMessagesCount();
+        this.cdr.detectChanges();
+    }
+
 
     ngOnDestroy(): void {
         this.subscriptions.forEach(s => s.unsubscribe());

702ad7ca0ed379052c271e66f58de4f0d17c2454
liamw6367@gmail.com <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>
2022-02-18 09:03:15 +0400

    Removed unnecessary mongo-chat folder with its components

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -5,17 +5,12 @@ import {ChatRoutingModule} from './chat-routing.module';
 import {ShowChatroomsComponent} from './show-chatrooms/show-chatrooms.component';
 import {ShowMessagesComponent} from './show-messages/show-messages.component';
 import {SharedModule} from '@shared/shared.module';
-import {DirectMongoChatComponent} from './show-messages/mongo-chat/direct-mongo-chat/direct-mongo-chat.component';
-import { GroupMongoChatComponent } from './show-messages/mongo-chat/group-mongo-chat/group-mongo-chat.component';
-import {SharedChatHelper} from '@core/helpers/shared-chat-helper';
 
 
 @NgModule({
     declarations: [
         ShowChatroomsComponent,
         ShowMessagesComponent,
-        DirectMongoChatComponent,
-        GroupMongoChatComponent
     ],
     imports: [
         CommonModule,

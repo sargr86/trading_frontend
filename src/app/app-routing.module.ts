28a646058c95ba80cd0b35c4bdbdc76e462857ad
liamw6367@gmail.com <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>
2022-02-25 14:35:13 +0400

    Refactored "user" urls to "users" urls

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -36,7 +36,7 @@ const routes: Routes = [
         canActivate: [NonAuthGuard]
     },
     {
-        path: 'user',
+        path: 'users',
         loadChildren: () => import('./users/users.module').then(m => m.UsersModule),
         canActivate: [AuthGuard]
     },

ab629c825100e4fa1361ae7e6e86aee63b69bf14
liamw6367@gmail.com <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>
2022-03-07 13:50:33 +0400

    Moved chat groups service methods to groups service

--- src/app/core/services/groups.service.ts
+++ src/app/core/services/groups.service.ts
@@ -12,19 +12,51 @@ export class GroupsService {
     ) {
     }
 
+    addGroup(params) {
+        return this.httpClient.post<any>(`${API_URL}groups/create-group`, params);
+    }
+
+    getGroupMembers(params) {
+        return this.httpClient.get<any>(`${API_URL}groups/get-group-members`, {params});
+    }
+
+    addGroupMembers(params) {
+        return this.httpClient.post<any>(`${API_URL}groups/add-group-members`, params);
+    }
+
+    removeGroupMember(params) {
+        return this.httpClient.delete<any>(`${API_URL}groups/remove-group-member`, {params});
+    }
+
+    removeGroup(params) {
+        return this.httpClient.delete<any>(`${API_URL}groups/remove-group`, {params});
+    }
+
+    leaveGroup(params) {
+        return this.httpClient.delete<any>(`${API_URL}groups/leave-group`, {params});
+    }
+
+    acceptGroupJoin(params) {
+        return this.httpClient.put<any>(`${API_URL}groups/accept-join-group`, params);
+    }
+
+    declineGroupJoin(params) {
+        return this.httpClient.put<any>(`${API_URL}groups/decline-join-group`, params);
+    }
+
     joinGroup(params) {
-        return this.httpClient.post<any>(`${API_URL}chat/group/join-group`, params);
+        return this.httpClient.post<any>(`${API_URL}groups/join-group`, params);
     }
 
     confirmGroupJoin(params) {
-        return this.httpClient.put<any>(`${API_URL}chat/group/confirm-join-group`, params);
+        return this.httpClient.put<any>(`${API_URL}groups/confirm-join-group`, params);
     }
 
     ignoreGroupJoin(params) {
-        return this.httpClient.put<any>(`${API_URL}chat/group/ignore-join-group`, params);
+        return this.httpClient.put<any>(`${API_URL}groups/ignore-join-group`, params);
     }
 
     getGroupByCustomName(params) {
-        return this.httpClient.get<any>(`${API_URL}chat/group/get-group-by-name`, {params});
+        return this.httpClient.get<any>(`${API_URL}groups/get-group-by-name`, {params});
     }
 }
